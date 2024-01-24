1e1a4adedda0470be4c4e010dc8f0e991057a465
imareev <101383082+imareev@users.noreply.github.com>
2024-01-25 00:56:59 +1000

    hw16(2)

--- hw16/src/shared/CommentForm/CommentForm.tsx
+++ hw16/src/shared/CommentForm/CommentForm.tsx
@@ -1,31 +1,19 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
+import { atom, useSetRecoilState } from 'recoil';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import style from './CommentForm.css';
 
-import { makeAutoObservable } from 'mobx';
-import { observer } from 'mobx-react-lite';
+// Создаем атом для хранения значения комментария
+const commentState = atom<string>({
+  key: 'commentState',
+  default: 'initial comment',
+});
 
-class Comment {
-  value = 'mobxe';
+export const CommentForm: React.FC = () => {
+  const setComment = useSetRecoilState(commentState);
 
-  constructor() {
-    makeAutoObservable(this);
-  }
-
-  updateValue(newValue: string) {
-    this.value = newValue;
-  }
-}
-
-const myComment = new Comment();
-
-
-function handleChange(value: string) {
-  myComment.updateValue(value);
-}
-
-export const CommentForm = observer(() => {
   const Schema = Yup.object({
     comment: Yup.string()
       .min(3, 'В строке должно быть больше трёх символов')
@@ -36,32 +24,28 @@ export const CommentForm = observer(() => {
     <div>
       <Formik
         initialValues={{
-          comment: myComment.value,
+          comment: '', 
         }}
         validationSchema={Schema}
         onSubmit={(values) => {
           alert('Comment submitted!');
         }}
       >
-        {({ errors, touched, values, handleChange: formikHandleChange }) => (
+        {({ handleChange, values }) => (
           <Form className={style.form}>
             <Field
               type="text"
               name="comment"
               className={style.input}
-              onChange={(e:any) => {
-                formikHandleChange(e);
-                handleChange(e.target.value); // Call your custom handleChange function
-              }}
+              onChange={handleChange}
             />
             <ErrorMessage name="comment" component="div" />
             <button type="submit" className={style.button}>
               Submit
             </button>
-
           </Form>
         )}
       </Formik>
     </div>
   );
-});
+};
